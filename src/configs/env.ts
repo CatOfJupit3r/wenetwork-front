0e96dea038d84fa89b7484ca8e83069c213616e9
Roman Barmak <roman.barmak@uplab.io>
2025-03-24 22:30:16 +0200

    revert: remove debug

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,5 +1,4 @@
-export const VITE_APP_BACKEND_URL: string = import.meta.env.VITE_APP_BACKEND_URL;
-console.log(import.meta.env);
+export const VITE_APP_BACKEND_URL: string = import.meta.env.VITE_BACKEND_URL;
 if (!VITE_APP_BACKEND_URL) {
     throw new Error('VITE_APP_BACKEND_URL is not set');
 }
