a9c21a1dd1ff2208605f69d35b449973ad458246
Roman Barmak <roman.barmak@uplab.io>
2025-03-24 22:18:16 +0200

    feat: rename var to VITE_APP_BACKEND_URL

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,4 +1,4 @@
-export const VITE_APP_BACKEND_URL: string = import.meta.env.VITE_BACKEND_URL;
+export const VITE_APP_BACKEND_URL: string = import.meta.env.VITE_APP_BACKEND_URL;
 if (!VITE_APP_BACKEND_URL) {
     throw new Error('VITE_APP_BACKEND_URL is not set');
 }
